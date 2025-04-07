9f6cd79979d1bfd12ce4f98517bc23a1790691ac
salam46khan <salam46.khan@gmail.com>
2025-04-08 00:07:07 +0600

    add feacture section

---
+++ src/components/HomeCompo/Feature.jsx
@@ -0,0 +1,31 @@
+import { CiCoffeeBean } from "react-icons/ci";
+import { LiaAwardSolid } from "react-icons/lia";
+import { TbAward } from "react-icons/tb";
+import { VscCoffee } from "react-icons/vsc";
+
+const Feature = () => {
+    return (
+        <div className="feature py-10 px-3">
+            <div className="container flex flex-col md:flex-row gap-3">
+                <div className="p-5">
+                    <VscCoffee className="text-Secondary text-5xl"/>
+                    <h2 className="font-subTitle text-xl text-primaryColor py-2">Awesome Aroma</h2>
+                    <p>You will definitely be a fan of the design & aroma of your coffee</p>
+                </div>
+                <div className="p-5">
+                    <LiaAwardSolid className="text-Secondary text-5xl"/>
+                    <h2 className="font-subTitle text-xl text-primaryColor py-2">High Quality</h2>
+                    <p>We served the coffee to you maintaining the best quality</p>
+                </div>
+                <div className="p-5">
+                    <CiCoffeeBean className="text-Secondary text-5xl"/>
+                    <h2 className="font-subTitle text-xl text-primaryColor py-2">Pure Grades</h2>
+                    <p>The coffee is made of the green coffee beans which you will love</p>
+                </div>
+                
+            </div>
+        </div>
+    );
+};
+
+export default Feature;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Banner from '../components/HomeCompo/Banner';
+import Feature from '../components/HomeCompo/Feature';
 
 const Home = () => {
     return (
         <div>
             <Banner></Banner>
+            <Feature></Feature>
         </div>
     );
 };
